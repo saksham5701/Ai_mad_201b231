1552ab426294fa6ca7c4af51393dae57ad5f460c
saksham <saksham5701@gmail.com>
2022-05-28 10:50:50 +0530

    Correct successfully set

--- app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
+++ app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
@@ -30,7 +30,8 @@ public class MainActivity extends AppCompatActivity {
     int []performance={-1,-1,-1,-1,-1,-1}; //score of a game is updated in this array
     int []score={-1,-1,-1}; //score of each match is updated in this array. A total of three matches in a game
     String operators[]={"+","-","*","/"};
-    int correctButton=0; //which button will have the correct answer (tag of that button)
+    int correctButton=0;
+     //which button will have the correct answer (tag of that button)
     Random random=new Random(); //You will generate randdom alegebra questions
     TextView textView2;
     Button button1,button2,button3,button4;
@@ -73,12 +74,48 @@ public class MainActivity extends AppCompatActivity {
 
         int operand1 = random.nextInt(10);
         int operand2= random.nextInt(10);
+        int correctButton=random.nextInt(4);
 
         //check is operand2 is not zero; otherwise in case of division-divide by zero error will come
         String operator = operators[random.nextInt(4)];
         textView2.setText(operand1 + operator + operand2);
 
       // Your code here, to diplay correct and incorrect options on the buttons
+        int Correct_ans=-100;
+        if(operator.equals("+"))
+            Correct_ans=operand1+operand2;
+        else if(operator.equals("-"))
+            Correct_ans=operand1-operand2;
+        else if(operator.equals("/"))
+            Correct_ans=operand1*operand2;
+        else
+            Correct_ans=operand1*operand2;
+
+        if(correctButton==0) {
+            button1.setText(Correct_ans + "");
+            button2.setText(Correct_ans + 1 + "");
+            button3.setText(Correct_ans - 1 + "");
+            button4.setText(Correct_ans + 2 + "");
+        }
+        if(correctButton==1){
+            button1.setText(Correct_ans+1+"");
+            button2.setText(Correct_ans+"");
+            button3.setText(Correct_ans-1+"");
+            button4.setText(Correct_ans+2+"");}
+
+        if(correctButton==2){
+            button1.setText(Correct_ans+1+"");
+            button2.setText(Correct_ans-1+"");
+            button3.setText(Correct_ans+"");
+            button4.setText(Correct_ans+2+"");}
+
+        else{
+            button1.setText(Correct_ans + 1 + "");
+            button2.setText(Correct_ans - 1 + "");
+            button3.setText(Correct_ans + 2 + "");
+            button4.setText(Correct_ans + "");
+        }
+
 
         if(matchCounter==3){    // if three matches are completed updatee the perfomrance in sharedpreferences
 
